6341807e26eacca88de4d9bcee6d47bbffceb43c
Andres Montoya Vargas <andresmontoyavargas.av@gmail.com>
2025-11-05 17:27:19 -0600

    Añadidas validaciones

--- Models/Mascota.cs
+++ Models/Mascota.cs
@@ -1,14 +1,33 @@
-﻿namespace PetCare.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PetCare.Models
 {
     public class Mascota
     {
-        public string PetName { get; set; }
-        public string Especie {  get; set; }
-        public string Raza { get; set; }
+        [Required(ErrorMessage = "Debe ingresar un nombre para poder referirse a la mascota")]
+        [StringLength(2, ErrorMessage = "El nombre debe tener al menos 2 caracteres")]
+        public string PetName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Debe ingresar una especie para la mascota")]
+        public string Especie {  get; set; } = string.Empty;
+
+        public string Raza { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Para poder agregar a la persona es necesario ingresar la edad")]
+        [Range(1, 120, ErrorMessage = "La edad debe estar entre 0 y 120 años")]
         public int Edad { get; set; }
-        public string OwnerName { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar el nombre del dueño")]
+        [StringLength(3, ErrorMessage = "El nombre debe tener al menos 3 caracteres")]
+        public string OwnerName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Se debe establecer un contacto (número telefónico")]
+        [Range(10000000, 99999999, ErrorMessage = "El número debe tener exactamente 8 dígitos.")]
         public int OwnerTel  { get; set; }
-        public DateTime DateIn {  get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar la fecha de ingreso de la mascota")]
+        [DataType(DataType.Date)]
+        public DateTime DateIn {  get; set; } = DateTime.Today;
 
 
     }
