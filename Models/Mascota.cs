3d180e9cff80e4da07c640e036ab8831aae4a572
Andres Montoya Vargas <andresmontoyavargas.av@gmail.com>
2025-11-04 22:25:08 -0600

    Agregar archivos de proyecto.

---
+++ Models/Mascota.cs
@@ -0,0 +1,15 @@
+﻿namespace PetCare.Models
+{
+    public class Mascota
+    {
+        public string PetName { get; set; }
+        public string Especie {  get; set; }
+        public string Raza { get; set; }
+        public int Edad { get; set; }
+        public string OwnerName { get; set; }
+        public int OwnerTel  { get; set; }
+        public DateTime DateIn {  get; set; }
+
+
+    }
+}
