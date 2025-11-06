5960b6f564cd1dd0eb19169d103b2638d04e7a8c
Andres Montoya Vargas <andresmontoyavargas.av@gmail.com>
2025-11-06 16:18:32 -0600

    Creación de métodos en el controlador para utilizar modelo y redirigir a views

--- Controllers/MascotasController.cs
+++ Controllers/MascotasController.cs
@@ -4,12 +4,37 @@ using PetCare.Models;
 
 namespace PetCare.Controllers
 {
+    //GET
     public class MascotasController : Controller
     {
         public IActionResult Index()
         {
             var mascotas = PetsRepo.GetAllPets();
-            return View();
+            return View(mascotas);
+        }
+
+        public IActionResult Create()
+        {
+            return View(new Mascota());
+        }
+
+        //POST
+        [HttpPost]
+        public IActionResult Create(Mascota model)
+        {
+            if (model.DateIn > DateTime.Today)
+            {
+                ModelState.AddModelError("DateIn", "La fecha de ingreso no puede ser futura");
+            }
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            PetsRepo.AddPet(model);
+
+            TempData["Mensaje"] = $"Mascota {model.PetName} agregada correctamente";
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
