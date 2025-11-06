5b897b451498ce88aa340070f1eef877d9f93112
Andres Montoya Vargas <andresmontoyavargas.av@gmail.com>
2025-11-05 18:47:08 -0600

    Creación del controlador

--- Controllers/MascotasController.cs
+++ Controllers/MascotasController.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PetCare.Data;
+using PetCare.Models;
 
 namespace PetCare.Controllers
 {
-    public class MascotasController1 : Controller
+    public class MascotasController : Controller
     {
         public IActionResult Index()
         {
+            var mascotas = PetsRepo.GetAllPets();
             return View();
         }
     }
