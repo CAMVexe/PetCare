4d5a53a0e64eab84522a760830f5b890e6bd4660
Andres Montoya Vargas <andresmontoyavargas.av@gmail.com>
2025-11-05 18:03:43 -0600

    Creación de repositorio

---
+++ Controllers/MascotasController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetCare.Controllers
+{
+    public class MascotasController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
